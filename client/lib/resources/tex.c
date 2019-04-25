44c13b403cafb7868ddde390ca8a742595390c06
Coestaris <vk_vm@ukr.net>
2019-04-25 20:14:49 +0300

    First game run using oil

--- client/lib/resources/tex.c
+++ client/lib/resources/tex.c
@@ -3,6 +3,7 @@
 //
 
 #include "tex.h"
+#include "../../../lib/oil/oil.h"
 
 tex2d* createTex(char *fn, int uid, int scope, vec_t center, int mode)
 {
@@ -123,12 +124,13 @@ void loadTex(tex2d* tex)
 
     for(int i = 0; i < tex->framesCount; i++)
     {
-        GLuint id = SOIL_load_OGL_texture (
-                tex->fns[i],
-                SOIL_LOAD_RGBA,
-                SOIL_CREATE_NEW_ID,
-                SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_MULTIPLY_ALPHA
-        );
+        GLuint id = oilTextureFromFile(tex->fns[i], GL_RGBA, GL_UNSIGNED_BYTE);
+        if(!id)
+        {
+            printf("Unable to load texture %s\n", tex->fns[i]);
+            oilPrintError();
+            exit(EXIT_FAILURE);
+        }
 
         if(tex->framesCount == 1)
         {
